7748a7f1c3a39400bf832176ff8250c3e3e74b79
mhdnihas <muhammednihas8589@gmail.com>
2023-12-21 11:41:00 +0530

    edit

--- statitics/mean.py
+++ statitics/mean.py
@@ -1,7 +1,9 @@
 import numpy as np 
 from scipy import stats
-arr=[1,2,3,3,4,5,6]
+arr=[1,2,3,3,4,5,6,10]
 mean=np.mean(arr)
 print("mean=",mean)
 mode=stats.mode(arr)
 print(mode)
+median=np.median(arr)
+print("median=",median)
